5f4f8e7f287786cb91d23c81c3643d7923755f1c
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2024-01-11 10:52:56 -0800

    docker set-up

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,89 @@
 const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 const mongoose = require('mongoose');
 
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
 const app = express();
-const PORT = 8000;
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false })); // <-- Add this line
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
+
+// Connect all the routes
+app.use(routes);
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/your-database-name', {
+mongoose.connect('mongodb://localhost:27017/Wake-Up', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -20,5 +98,5 @@ db.once('open', () => {
 // Define routes and middleware as needed
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
